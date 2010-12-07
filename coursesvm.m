a895c5b39db045a649b11f2ac48c331bf6f25a58
Sharon <shirokiryu@gmail.com>
2010-12-07 11:35:33 -0800

    update feature calculator

--- coursesvm.m
+++ coursesvm.m
@@ -1,17 +1,33 @@
-addpath('C:/Users/sharon/Desktop/liblinear-1.7/matlab');
-addpath('C:/Users/sharon/Desktop/liblinear-1.7/windows');
+%addpath('C:/Users/sharon/Desktop/liblinear-1.7/matlab');
+%addpath('C:/Users/sharon/Desktop/liblinear-1.7/windows');
+addpath('C:/Users/sharon/Desktop/libsvm-mat-3.0-1/libsvm-mat-3.0-1');
 
 username = 'root';
-pass = 'pass';
+pass = '';
 databasepath = 'jdbc:mysql://localhost:3306/stanford';
 conn = database('stanford', username, pass,'com.mysql.jdbc.Driver',databasepath);
+
+includeRatings = 1;
+includeTranscript = 1;
+includeMajor = 1;
+includeUnits = 1;
+includeWorkload = 1;
+includeHours = 1;
+includeNumCoursesTaken = 1;
+includeDeptGrades = 1;
 
 %gradeId to number mapping, A(1-3):5, B(4-6):4, C(7-9):3, D(10-12):2,
 %F(13):1, Other(14-17):0
 %TODO: maybe make a Credit=C and NoCredit=F??
-gradeMap = [5,5,5,4,4,4,3,3,3,2,2,2,1,0,0,0,0];
-%gradeMap2 = [5,5,5,4,4,4,3,3,3,2,2,2,1,0,0,0,0];
-gradeMap2 = [1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
+%gradeMap = [5,5,5,4,4,4,3,3,3,2,2,2,1,0,0,0,0];
+%gradeMap = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+%labelMap = [5,5,5,4,4,4,3,3,3,2,2,2,1,0,0,0,0];
+gradeMap = 17:-1:1;
+gradeMap(14:17) = 0;
+%labelMap = 17:-1:1;
+labelMap = [1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
+%labelMap = [-1,-1,-1,1,1];
+%gradeMap = [1,2,3,4,5];
 
 %record threshold
 thresh = 50;
@@ -22,19 +38,36 @@ result = fetch(curs);
 majorids = cell2mat(result.Data);
 numMajors = size(majorids, 1);
 
+%get the department ids
+curs = exec(conn, 'SELECT distinct departmentid FROM `unifiedhistorygrades` WHERE grade<14');
+result = fetch(curs);
+deptids = cell2mat(result.Data);
+numDepts = size(deptids,1);
+
 %get the course names/ids
-curs = exec(conn, ['SELECT courseid FROM `studenthistoryt` WHERE grade<14 group by courseid having count(*)>=',num2str(thresh)]);
+curs = exec(conn, ['SELECT courseid FROM `studenthistoryt` WHERE grade<14 group by courseid having count(*)>=',num2str(thresh),' ORDER BY count(*) desc']);
+%curs = exec(conn, ['SELECT courseid FROM `studenthistoryt` WHERE 1 group by courseid having count(*)>=',num2str(thresh)]);
 result = fetch(curs);
 courseids = cell2mat(result.Data);
 numCourses = size(courseids,1);
 
 errors = zeros(numCourses,2); %[training error, testing error]
 
+courseacc = zeros(numCourses, 5);
+
+filter = [6441,7843,23092,28713,8739,11411,15497,5600,4268,29783,30703,19328,15262,25919,21740];
 %for each courseid, find the students who have taken the course
-for i=1:1%numCourses
+for i=25:40
     courseid = courseids(i);
+    
+    %if (courseid ~= 10853 && courseid ~= 3634)
+    %if (min(size(find(filter==courseid)))==0)
+    %    continue;
+    %end
     %find students who have taken the course and their grades
-    curs = exec(conn, ['SELECT studentid, grade, termid FROM `studenthistoryt` WHERE courseid=',num2str(courseid),' and grade<14']);
+    %curs = exec(conn, ['SELECT studentid, grade, termid FROM `studenthistoryt` WHERE courseid=',num2str(courseid),' and grade<14 ORDER BY grade']);
+    curs = exec(conn, ['SELECT studentid, grade, termid FROM `studenthistoryt` WHERE courseid=',num2str(courseid),' and grade!= 17 and grade !=14 ORDER BY grade']);
+  
     result = fetch(curs);
     students = cell2mat(result.Data(:,1));
     grades = cell2mat(result.Data(:,2)); 
@@ -43,10 +76,9 @@ for i=1:1%numCourses
     %for each student, calculate their feature vectors
     numStudents = size(students,1);
     
-    %features = zeros(numStudents, numCourses+numMajors+1);
-    features = zeros(numStudents, numMajors+1);
+    features = zeros(numStudents, 2*numCourses+numMajors+3+2*numDepts);
+    %features = zeros(numStudents, numMajors+3);
     labels = zeros(numStudents,1);
-    nonzeroGrades = 0;
     
     for j=1:numStudents
         %find the courses the student has previously taken
@@ -54,6 +86,9 @@ for i=1:1%numCourses
         term = terms(j);
         
         studenthistory = zeros(1,numCourses);
+        ratingshistory = zeros(1,numCourses);
+        deptgradeshistory = zeros(1,numDepts);
+        deptgradesrecent = zeros(1,numDepts);
         
         %get start date
         curs = exec(conn, ['select startdate from termst where termid=',num2str(term)]);
@@ -64,7 +99,35 @@ for i=1:1%numCourses
                 continue;
         end
         startDate = datenum(startDate.Data);
-
+        
+        %get student's dept gpa
+        curs = exec(conn, ['select sum(points)/count(*), departmentid from unifiedhistorygrades where grade<14 and studentid=',num2str(student),' and startdate<"', datestr(startDate,'yyyy-mm-dd'),'" group by departmentid']);
+        result = fetch(curs);
+        if (size(result.Data,2) == 2)
+            currGPA = cell2mat(result.Data(:,1));
+            currDept = cell2mat(result.Data(:,2));
+            
+            for d=1:size(currDept,1)
+                %find dept index
+                index = find(deptids==currDept(d));
+                deptgradeshistory(index) = currGPA(d);
+            end
+        end
+        
+        %get student's recent dept gpa, within 300 days
+        curs = exec(conn, ['select sum(points)/count(*), departmentid from unifiedhistorygrades where grade<14 and studentid=',num2str(student),' and startdate<"', datestr(startDate,'yyyy-mm-dd'),'" and startdate>="', datestr(startDate-300,'yyyy-mm-dd'),'" group by departmentid']);
+        result = fetch(curs);
+        if (size(result.Data,2) == 2)
+            currGPA = cell2mat(result.Data(:,1));
+            currDept = cell2mat(result.Data(:,2));
+            
+            for d=1:size(currDept,1)
+                %find dept index
+                index = find(deptids==currDept(d));
+                deptgradesrecent(index) = currGPA(d);
+            end
+        end
+        
         %get student's major (TODO: Get department instead?)
         curs = exec(conn, ['select studyprogramid from studentstudiest where studentid=',num2str(student)]);
         result = fetch(curs);
@@ -76,82 +139,112 @@ for i=1:1%numCourses
         end
         
         %get student's current quarter workload 
-        curs = exec(conn, ['SELECT SUM(units) FROM `studenthistoryt` WHERE studentid=',num2str(student),' and termid=',num2str(term)]);
+        curs = exec(conn, ['SELECT SUM(units), COUNT(*) FROM `studenthistoryt` WHERE studentid=',num2str(student),' and termid=',num2str(term)]);
         result = fetch(curs);
-        totalUnits = cell2mat(result.Data);
+        totalUnits = max([cell2mat(result.Data(1)), cell2mat(result.Data(2))]); 
         
-        curs = exec(conn, ['SELECT courseid, grade, termid FROM `studenthistoryt` WHERE studentid=',num2str(student),' and grade<14 and termid!=',num2str(term)]);
-        
+        curs = exec(conn, ['SELECT courseid, units FROM `studenthistoryt` WHERE studentid=',num2str(student),' and termid=',num2str(term)]);
         result = fetch(curs);
-        if (size(result.Data,2) < 3)
+        currCourses = cell2mat(result.Data(:,1));
+        currUnits = cell2mat(result.Data(:,2));
+        
+        totalHours = 0;
+
+        for k=1:size(currCourses,1)
+            curs = exec(conn, ['SELECT SUM((workload_id*num_responses))/SUM(num_responses), term_id  FROM `workload_courses` where course_id=',num2str(courseid),' GROUP BY term_id']);
+            result = fetch(curs);
+            
+             if (size(result.Data,2) < 2)
+                 totalHours = totalHours + currUnits(k);
+                 continue;
+             end
+            
+            workloads = cell2mat(result.Data(:,1));
+            workloadTerms = cell2mat(result.Data(:,2));
+        
+            sumWorkloads = 0;
+            totalValidWorkloads = 0;
+            for n=1:size(workloadTerms,1)
+                curs = exec(conn, ['select startdate from termst where termid=',num2str(workloadTerms(n))]);
+                prevDate = fetch(curs);
+                if (strcmp(cell2mat(prevDate.Data),'No Data'))
+                    continue;
+                end
+                prevDate = datenum(prevDate.Data);
+               % if (prevDate > startDate)
+               %     continue;
+               % end
+                sumWorkloads = sumWorkloads + workloads(n);
+                totalValidWorkloads = totalValidWorkloads+1;
+            end
+            if (totalValidWorkloads > 0)
+                totalHours = totalHours + (sumWorkloads/totalValidWorkloads);
+            else
+                totalHours = totalHours + max([currUnits(k),1]);
+            end
+        end
+        
+        coursesTaken = 0;
+        curs = exec(conn, ['SELECT courseid, grade, termid, rating FROM `unifiedhistorygrades` WHERE studentid=',num2str(student),' and startDate<"', datestr(startDate,'yyyy-mm-dd'),'"']);
+        %curs = exec(conn, ['SELECT courseid, grade, termid FROM `studenthistoryt` WHERE studentid=',num2str(student),' and grade<14 and termid=',num2str(term)]);
+        
+        %curs = exec(conn, ['SELECT courseid, rating, termid FROM `studenthistoryt` WHERE studentid=',num2str(student),' and rating>0 and termid!=',num2str(term)]);
+        result = fetch(curs);
+        if (size(result.Data,2) < 4)
             %student has not taken any courses, continue
-            %features(j,:) = [studenthistory majorfeatures totalUnits];
-            features(j,:) = [majorfeatures totalUnits];
-            labels(j) = gradeMap2(grades(j));
+            features(j,:) = [deptgradesrecent deptgradeshistory studenthistory ratingshistory majorfeatures totalUnits totalHours coursesTaken];
+            %features(j,:) = [majorfeatures totalUnits totalHours coursesTaken];
+            labels(j) = labelMap(grades(j));
             continue;
         end
         prevCourses = cell2mat(result.Data(:,1));
         prevGrades = cell2mat(result.Data(:,2));
         prevTerms = cell2mat(result.Data(:,3));
+        prevRatings = cell2mat(result.Data(:,4));
         
         %calculate the feature vector
         %[prev course grades | student's major | current
         %quarter workload in credits]
         %Currently overwriting grades if course taken more than once...
+
         for k=1:size(prevCourses,1)
             %ignore courses taken later or at the same time
-            curs = exec(conn, ['select startdate from termst where termid=',num2str(prevTerms(k))]);
-            prevDate = fetch(curs);
-            if (strcmp(cell2mat(prevDate.Data),'No Data'))
-                continue;
-            end
-            prevDate = datenum(prevDate.Data);
-            if (prevDate >= startDate)
-                continue;
-            end
             index = find(courseids == prevCourses(k));
             if (size(index,2)>0)
                 studenthistory(index) = gradeMap(prevGrades(k));
+                ratingshistory(index) = prevRatings(k);
             end
+            coursesTaken = coursesTaken + 1;
         end
         
         %find number of nonzero recorded grades (debugging purposes)
-        nonzeroGrades = nonzeroGrades + size(find(studenthistory>0),2);
-        %features(j,:) = [studenthistory majorfeatures totalUnits];
-        features(j,:) = [majorfeatures totalUnits];
-        labels(j) = gradeMap2(grades(j));
+        features(j,:) = [deptgradesrecent deptgradeshistory studenthistory ratingshistory majorfeatures totalUnits totalHours coursesTaken];
+        %features(j,:) = [majorfeatures totalUnits totalHours coursesTaken];
+        labels(j) = labelMap(grades(j));
         
     end  
-    nonzeroGrades = nonzeroGrades/numStudents;
     
     testerror = 0;
     trainerror = 0;
-    %leave one out cross-validation
-    for n=1:numStudents
-       %leave out student l, train on the rest
-       subset = features(1:n-1,:);
-       featurestrain = [subset; features(n+1:numStudents,:)];
-       
-       subsetl = labels(1:n-1);
-       labelstrain = [subsetl; labels(n+1:numStudents)];
-       
-       model = train(labelstrain, sparse(featurestrain));
-       
-       %training error
-       [ltrain, acctrain] = predict(labelstrain, sparse(featurestrain), model);
-       trainerror = trainerror + (100-acctrain);
-       
-       %test error
-       [ltest, acctest] = predict(labels(n,:), sparse(features(n,:)), model);
-       
-       testerror = testerror + (100-acctest);
-    end
+    labelstest = zeros(numStudents,1);
     
-    crossacc = train(labels, sparse(features), '-v 10');
+    %order = randperm(numStudents);
+    
+    libsvmwrite(['course',num2str(courseids(i))], labels, sparse(features));
+    numStudents
+    
+    %crossacc = train(labels, sparse(features), '-v 10');
+    %crossacc = svmtrain(labels, sparse(features), '-v 10  -w5 0.3');
+    %crossacc = svmtrain(labels, features, '-v 10 -t 0');
+    %model = svmtrain(labels, features, '-t 0');
+    %[ltrain, acctrain] = svmpredict(labels, features, model);
    
-    trainerror = trainerror/numStudents;
-    testerror = testerror/numStudents;
-    errors(i,:) = [trainerror testerror];
+    %trainerror = trainerror/numStudents;
+    %testerror = testerror/numStudents;
+    %errors(i,:) = [trainerror(1) testerror(1)];
     %model = train(labels, sparse(features));
     %[l,a] = predict(labels, sparse(features), model);
+    %traintest;
+    %courseacc(i,:) = [acc_train(1), crossacc, acc_test(1), a_acc, b_acc];
+    clearvars features;
 end
--- traintest.m
+++ traintest.m
@@ -1,51 +1,214 @@
-train_size = round(0.9*size(features,1));
-order = randperm(size(features,1));
+addpath('C:/Users/sharon/Desktop/libsvm-mat-3.0-1/libsvm-mat-3.0-1');
+addpath('C:/Users/sharon/My Documents/MATLAB/old');
 
-labels_train = zeros(train_size,1);
-labels_test = zeros(size(features,1)-train_size,1);
+%username = 'root';
+%pass = '';
+%databasepath = 'jdbc:mysql://localhost:3306/stanford';
+%conn = database('stanford', username, pass,'com.mysql.jdbc.Driver',databasepath);
 
-randomized_features = features;
-randomized_labels = labels;
+courseids = [1738, 3634, 6107, 10853, 12688, 17316, 20197, 21837, 23805, 31075, 31632, 1962, 6107, 16796, 21691, 23092, 23849, 29783];
+%courseids = [1738, 3634, 6107, 10853];
+%courseids = [1962 6107 16796 21691 23092 23849 29783];
 
-for f=1:size(features,1)
-   randomized_features(f,:) = features(order(f),:); 
-   randomized_labels(f) = labels(order(f));
-end
+%study programs
+%curs = exec(conn, 'SELECT studyprogramid FROM `studyprogramst`');
+%result = fetch(curs);
+%majorids = cell2mat(result.Data);
+numMajors = 76;%size(majorids, 1);
 
-next = train_size+1;
-training_set = randomized_features(1:train_size,:);
-test_set = randomized_features(next:size(features,1),:);
-labels_train = randomized_labels(1:train_size);
-labels_test = randomized_labels(next:size(features,1));
+%get the department ids
+%curs = exec(conn, 'SELECT distinct departmentid FROM `unifiedhistorygrades` WHERE grade<14');
+%result = fetch(curs);
+%deptids = cell2mat(result.Data);
+numDepts = 177;%size(deptids,1);
 
-model = svmtrain(labels_train, training_set, '-t 0');
-[ltr, acc_train] = svmpredict(labels_train, training_set, model);
+numCourses=403;
 
-[lte, acc_test] = svmpredict(labels_test, test_set, model);
+%Best to worst...recent department grades, overall department grades,
+%major, course workload, courses taken
+courseacc = zeros(size(courseids,2),5);
+numStudents = zeros(size(courseids,2),1);
+highprobacc = zeros(size(courseids,2),3);
+bestParams = zeros(size(courseids,2),2);
 
-svmtrain(labels, features, '-v 10 -t 0');
+baseline = zeros(size(courseids,2),1);
+baseline2 = zeros(size(courseids,2),1); %student's mean grade
 
-%get A label acc, and non-A label acc
-a_acc_sum = 0;
-num_as = size(find(labels_test>0),1);
-b_acc_sum = 0;
-num_bs = size(labels_test,1)-num_as;
-for f=1:size(labels_test,1)
-    if (labels_test(f)>0)
-        if (lte(f) == labels_test(f))
-            a_acc_sum = a_acc_sum+1;
-        end
-    else
-        if (lte(f) == labels_test(f))
-        	b_acc_sum = b_acc_sum+1;
+opt = 0; %train for acc or train for precision on a's
+
+for i=1:size(courseids,2)
+    [labels, features] = libsvmread(['concurrcourse',num2str(courseids(i))]);
+    
+    %get concurrent dept, concurrent courses
+    concurrDept = features(:,1:numDepts)./5.0;
+    concurrCourses = features(:,(numDepts+1):size(features,2));
+    
+    [labels, features] = libsvmread(['course',num2str(courseids(i))]);
+    
+    %scale the data
+    cursor = 1;
+    recentdept = features(:,1:numDepts)./4.33;
+    cursor = numDepts+1;
+    deptgrades = features(:,cursor:(cursor+numDepts-1))./4.33;
+    cursor = cursor+numDepts;
+    coursegrades = features(:,cursor:(cursor+numCourses-1))./17.0;
+    cursor = cursor+numCourses;
+    ratings = features(:,cursor:(cursor+numCourses-1))./5.0;
+    cursor= cursor+numCourses;
+    majorfeatures = features(:,cursor:(cursor+numMajors-1));
+    cursor = cursor+numMajors;
+    numCredits = features(:,cursor)./25.0;
+    cursor = cursor+1;
+    numHours = features(:,cursor)./25.0;
+    cursor = cursor+1;
+    numPrevCourses = features(:, cursor)./80.0;
+    
+
+    %C's to try
+    Cs = [128 64 32 16 8 4 2 1];
+    bestC = 1;
+    bestCrossAcc = 0;
+    
+    features = [recentdept concurrCourses coursegrades majorfeatures numHours numPrevCourses];
+
+    gamma = 1.0/size(features,2);
+    Gammas = [gamma 2*gamma 4*gamma 8*gamma 16*gamma];
+    
+    train_size = round(0.9*size(features,1));
+    order = randperm(size(features,1));
+
+    randomized_features = features;
+    randomized_labels = labels;
+
+    for f=1:size(features,1)
+       randomized_features(f,:) = features(order(f),:); 
+       randomized_labels(f) = labels(order(f));
+    end
+
+    next = train_size+1;
+    training_set = randomized_features(1:train_size,:);
+    test_set = randomized_features(next:size(features,1),:);
+    labels_train = randomized_labels(1:train_size);
+    labels_test = randomized_labels(next:size(features,1));
+    
+    
+    %Baseline is to guess the grade that happens most often
+    guess = 1;
+    if (sum(labels<0) > sum(labels>0))
+        guess = -1;
+    end
+    baseline_acc = sum(labels==guess)/size(labels,1);
+    
+    
+    %Baseline 2 is student's mean grade
+    
+    
+    best_model = 0;
+    best_crossacc = 0;
+    best_params = [1 1];
+    best_testacc = 0;
+    
+    if (opt == 0)
+        for c=1:size(Cs,2)
+            for g=1:size(Gammas,2)
+                %model = svmtrain(labels_train, training_set, '-t 0');
+
+                %crossacc = svmtrain(labels, features, '-v 10 -t 0');
+                %crossacc = svmtrain(labels, features, ['-v 10 -c ', num2str(Cs(c)), ' -g ', num2str(best_params(2))]);
+                crossacc = svmtrain(labels_train, training_set, ['-v 10 -c ', num2str(Cs(c)), ' -g ', num2str(Gammas(g))]);
+                %svmtrain(labels, features, '-v 10');
+
+                if (crossacc > best_crossacc) %|| (crossacc == best_crossacc && acc_test(1)>best_testacc ))
+                    %best_model = model;
+                    best_crossacc = crossacc;
+                    best_params = [Cs(c) Gammas(g)];
+                end
+            end
         end
     end
+    if (opt == 1)
+        for c=1:size(Cs,2)
+            for g=1:size(Gammas,2)
+                %do 10-fold cross validation
+                inds = 1;
+                chunk = size(training_set,1)/10.0;
+                
+                crossacc = 0;
+                for x=1:10
+                    inds = 1+floor(chunk*(x-1));
+                    inde = floor(inds+chunk-1);
+                    subset_test = training_set(inds:inde,:);
+                    labels_subset_test = labels_train(inds:inde);
+                    
+                    subset = [training_set(1:inds-1,:);training_set(inde+1:size(training_set,1),:)];
+                    labels_subset = [labels_train(1:inds-1);labels_train(inde+1:size(training_set,1))];
+                    
+                    model = svmtrain(labels_subset, subset, ['-c ', num2str(Cs(c)), ' -g ', num2str(Gammas(g))]);
+                    [lte, acc_test] = svmpredict(labels_subset_test, subset_test, model);
+                    
+                    prec = (1-opt_function(lte, labels_subset_test, opt));
+                    if (isnan(prec))
+                        prec = 1;
+                    end
+                    crossacc = crossacc + prec;
+                end
+                crossacc = crossacc/10.0;
+                    
+                if (crossacc > best_crossacc)
+                   best_crossacc = crossacc;
+                   best_params = [Cs(c) Gammas(g)];
+                end
+            end
+        end
+    end
+
+    model = svmtrain(labels_train, training_set, [' -c ', num2str(best_params(1)), ' -g ', num2str(best_params(2))]);
+
+    [ltr, acc_train] = svmpredict(labels_train, training_set, model);
+    
+    [lte, acc_test] = svmpredict(labels_test, test_set, model);
+    %a_acc = sum((lte==labels_test).*(labels_test>0))/sum(labels_test>0);
+    %b_acc = sum((lte==labels_test).*(labels_test<0))/sum(labels_test<0);
+    
+    %a_acc_train = sum((ltr==labels_train).*(labels_train>0))/sum(labels_train>0);
+    %b_acc_train = sum((ltr==labels_train).*(labels_train<0))/sum(labels_train<0);
+    
+    a_acc = sum((lte==labels_test).*(lte>0))/sum(lte>0);
+    b_acc = sum((lte==labels_test).*(lte<0))/sum(lte<0);
+    
+    a_rec = sum((lte==labels_test).*(labels_test>0))/sum(labels_test>0);
+    b_rec = sum((lte==labels_test).*(labels_test<0))/sum(labels_test<0);
+    
+    
+    a_acc_train = sum((ltr==labels_train).*(ltr>0))/sum(ltr>0);
+    b_acc_train = sum((ltr==labels_train).*(ltr<0))/sum(ltr<0);
+    
+    
+    crossacc = best_crossacc;
+    
+    thresh = 0.6;
+    
+    %acc_high = sum((lte==labels_test).*or(probte(:,1)>thresh, probte(:,2)>thresh))/sum(or(probte(:,1)>thresh, probte(:,2)>thresh));
+    %a_acc_high = sum((lte==labels_test).*(labels_test>0).*or(probte(:,1)>thresh, probte(:,2)>thresh))/sum((labels_test>0).*or(probte(:,1)>thresh, probte(:,2)>thresh));
+    %b_acc_high = sum((lte==labels_test).*(labels_test<0).*or(probte(:,1)>thresh, probte(:,2)>thresh))/sum((labels_test<0).*or(probte(:,1)>thresh, probte(:,2)>thresh));
+    
+    courseacc(i,:) = [ crossacc, acc_train(1), acc_test(1), a_acc_train, a_acc];
+  
+    %highprobacc(i,:) = [acc_high, a_acc_high, b_acc_high];
+    numStudents(i) = size(features,1);
+    
+    courseacc(isnan(courseacc))=-1;
+    %highprobacc(isnan(highprobacc))=-1;
+    bestParams(i,:) = best_params;
+    baseline(i) = baseline_acc;
+    
+    plot_error
 end
 
-%acc_train
-%acc_test
-a_acc = a_acc_sum/max([num_as, 1])
-b_acc = b_acc_sum/max([num_bs,1])
-
+courseacc
+numStudents
+bestParams
+[courseacc(:,1) baseline]
+%highprobacc
 
        
